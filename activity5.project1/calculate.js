11ba4df39bfa7f9cede79a43fe129ed5eabec367
Roberto <robequiron@gmail.com>
2021-04-13 21:29:13 +0100

    init

---
+++ activity5.project1/calculate.js
@@ -0,0 +1,16 @@
+class Calculate {
+
+
+    divCalculate = '<div style="background-color:red">Hola</div>'
+
+
+    constructor() {
+        let newDiv = document.createElement("div")
+        newDiv
+      
+    }
+
+
+
+
+}
